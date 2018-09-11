4d540acc958448b0142a31be00c91e72558bc1bc
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-11 09:04:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-var name = "Susan"
-var height = 70
+var name = "Joe"
+var height = "74"
+
+if (name != "Susan")
+{
+  var name = "Susan";
+}
+else
+{
+ console.log("the name is Susan") 
+}
+
+if (height != 70)
+{
+  var height = 70;
+}
+else
+{
+  console.log("the height is 70");
+}
 
 
 // Don't worry about this
